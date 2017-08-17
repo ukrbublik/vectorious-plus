cde6ae51e4240842222bf336dd77b47ca2495516
Denis Oblogin <ukrbublik@gmail.com>
2017-08-17 23:43:53 +0300

    Fixed benchmark for SpVector

--- benchmarks/sp_vector.js
+++ benchmarks/sp_vector.js
@@ -12,7 +12,7 @@
   var N = 1024,
       nz = 128,
       d = Vector.random(N),
-      s = new SpVector(N, new Float64Array(nz), new Int32Array(nz)),
+      s = new SpVector(new Float64Array(nz), new Int32Array(nz), {length: N}),
       sd;
 
   var val, i, tmp = {};
@@ -26,13 +26,13 @@
     tmp[i] = x;
   }
   sd = s.toVector();
-
+  
   console.log('d = Vector.random(' + N + ')');
   console.log('s - SpVector with ' + nz + ' nonzeros');
   console.log('sd - Vector (dense), has same values as s');
 
   suite
-    .add('s.dot(d)', function() { s.dot(d); }) //slower
+    .add('s.dot(d)', function() { s.dot(d); })
     .add('sd.dot(d)', function() { sd.dot(d); })
 
     .on('cycle', function (event) { console.log(String(event.target)); })
